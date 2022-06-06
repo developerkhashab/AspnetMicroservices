210dff448ec9e0640cdb7f18a3ee1cf98a96aeea
developerkhashab <mohammad.khashab.mk@gmail.com>
2022-06-06 23:41:09 +0300

    add redis ,create basket repository

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CasheSettings:ConnectionString");
+});
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,4 +19,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
 app.Run();
---
+++ src/Services/Basket/Basket.API/Repository/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repository
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart userName);
+        Task DeleteBasket(string userName);
+    }
+}
